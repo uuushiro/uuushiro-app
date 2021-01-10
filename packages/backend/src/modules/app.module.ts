f64d49939ef8a61384e6ff2db52ec77094a38cdc
uuushiro <yushiro.ma2ta2.21@gmail.com>
2021-01-10 16:49:01 +0900

    first commit (backend)

---
+++ packages/backend/src/modules/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from '../controllers/app.controller';
+import { AppService } from '../services/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'uuushiro-app',
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
